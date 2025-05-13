19d734051f6b658a0f907c56bb1369d91ea592fa
Sofia Ford <scfordw@bu.edu>
2025-05-13 13:07:08 -0500

    Adding some of the code from index.js into App.js and moving the necessary code from index.css to App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
@@ -6,6 +7,69 @@ function App() {
   const [infoText, setInfoText] = useState("Type a word in Finnish and press Enter");
   const [result, setResult] = useState(null);
   const [audioUrl, setAudioUrl] = useState("");
+
+  const translateWord = async(word) => {
+    try{
+      const res = await fetch("https://libretranslate.com/translate",{
+        method: "POST",
+        body: JSON.stringify({
+          q: word,
+          source: "fi",
+          target: "en",
+          format: "text",
+        }),
+        headers: {"Content-Type": "application/json"},
+      });
+
+      const data = await res.json();
+      return data.translatedText;
+    } 
+    catch(err){
+      console.error("Translated error:", err);
+      setInfoText("Translation error. Try again.");
+      return null;
+    }
+  };
+
+  const fetchDefinition = async(word) => {
+    try {
+      setInfoText(`Searching for the meaning of "${input}"...`);
+      setResult(null);
+      setAudioUrl("");
+
+      const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+      const data = await res.json();
+
+      if (data.title){
+        setInfoText("No meaning found.");
+        setResult({word, meaning: "N/A", audio: "None"});
+        return;
+      }
+
+      const firstMeaning = data[0].meanings[0].definitions[0].definition;
+      const audio = data[0].phonetics.find(p => p.audio)?.audio;
+
+      setResult({
+        word: data[0].word,
+        meaning: firstMeaning,
+      });
+
+      if (audio){
+        setAudioUrl(audio);
+      }
+      setInfoText("");
+    }
+    catch(err){
+      console.error("Definition fetch error:", error);
+      setInfoText("Error fetching definition.");
+    }
+  };
+
+  return(
+    <div className="container">
+      <h1></>
+    </div>
+  );
 }
 
 export default App;
